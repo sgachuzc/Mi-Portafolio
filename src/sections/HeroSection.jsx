1acce6d604ce1cf3cf763276aeed439659fa9686
Sergio Gachuz <sergioegch@hotmail.com>
2024-08-01 15:39:58 -0600

    Hero Section

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,7 +1,23 @@
-export const HeroSection = () => {
+import { Link } from 'react-router-dom';
+import { ReactTyped } from 'react-typed'; 
+import '../styles/sections/hero.css'
+
+export const HeroSection = () => { 
+  const textToType = [
+    "Desarrollador Web",
+    "Ing. en Sistemas Computacionales",
+  ]
   return (
-    <section>
-      <h1>Sergio Gachúz</h1>
+    <section id='hero'>
+      <span>Hola🫡, soy</span>
+      <h1>Sergio<br />Gachúz</h1>
+      <ReactTyped strings={ textToType } typeSpeed={ 40 } backSpeed={ 30 } />
+      <div className="section-links">
+        <Link className='link' target='_blank' to={'../../public/Curriculum.pdf'} download>
+          Descargar CV 
+          <img src={'../../public/download.svg'} alt="" />
+        </Link>
+      </div>
     </section>
   )
 }
